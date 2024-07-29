f5c78ee900e3d2c2061695952c0622f772062bff
Mustapha Abdulkareem <aphatheology@gmail.com>
2024-07-29 15:55:15 +0100

    add port

--- app.py
+++ app.py
@@ -56,4 +56,5 @@ def diagnose():
         return jsonify({"error": "Please enter the symptoms"}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
